529193cc8c9c3b0f369839ec63cbc80f96067761
angeloalmanza <angeloalmanza26@gmail.com>
2024-11-11 17:52:36 +0100

    feat: implementazione input e confronto tra i numeri random e input utente

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ const startGame = document.getElementById("start-game");
 const numbers = document.getElementById("number");
 const counterElem = document.getElementById("counter");
 const startBtn = document.getElementById("start-btn");
+const inputArea = document.getElementById("input-area");
+const inputs = document.querySelectorAll("input");
+const userBtn = document.getElementById("user-btn");
+const result = document.getElementById("result");
 
 const randomNumbers = [];
 
@@ -35,8 +39,36 @@ startBtn.addEventListener("click", function () {
             intervalId = null;
             setTimeout(function(){
                 startGame.classList.add("d-none");
+                inputArea.classList.remove("d-none");
             }, 1000);
         }
       }, 1000);
     }
+});
+
+// Confronto numeri random con numeri dell'utente e stampa del risultato
+userBtn.addEventListener("click", function () {
+    const userNumbers = [];
+    const correctNumbers = [];
+    const wrongNumbers = [];
+
+    for (let i = 0; i < inputs.length; i++) {
+        const userGuess = parseInt(inputs[i].value);
+        userNumbers.push(userGuess);
+
+        if (randomNumbers.includes(userGuess)) {
+            correctNumbers.push(userGuess);
+        }else{
+            wrongNumbers.push(userGuess);
+        }
+    }
+
+    if (correctNumbers.length > 0) {
+        result.innerHTML = `Hai indovinato ${correctNumbers.length} numero/i: ${correctNumbers.join(", ")}`;
+    } else {
+        result.innerHTML = "Non hai indovinato nessun numero.";
+    }
+
+    result.innerHTML += `<div>Numeri inseriti: ${userNumbers.join(", ")}</div>`;
+    result.innerHTML += `<div>Numeri sbagliati: ${wrongNumbers.join(", ")}</div>`;
 });
